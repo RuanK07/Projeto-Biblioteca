a1b50f4d8dce324e05efc594e4a3197565431328
Ruan Kennedy <ruank07@gmail.com>
2022-08-23 10:40:34 -0300

    Local project created

---
+++ src/Entities/Administrator.java
@@ -0,0 +1,18 @@
+package Entities;
+
+public class Admin{
+    
+    private String name;
+    private String password;
+
+    public Employee() {
+    }
+
+    public Employee(Integer id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+
+}
